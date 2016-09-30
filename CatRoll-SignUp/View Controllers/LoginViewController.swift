96b4d8cce1425bbda8a662334637cd4623d118c4
Louis Tur <louis.tur@gmail.com>
2016-09-30 18:07:35 -0400

    finishes character count validations, fixes some things in storyboard

--- CatRoll-SignUp/View Controllers/LoginViewController.swift
+++ CatRoll-SignUp/View Controllers/LoginViewController.swift
@@ -29,6 +29,60 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func didTapLogin(_ sender: UIButton) {
         print("did tap button")
+        print("Textfields are valid: \(textFieldsAreValid())")
+    }
+    
+    // MARK: - Validations
+    func textFieldsAreValid() -> Bool {
+        
+        // 1. some set up
+        let textFields: [UITextField] = [self.nameTextField, self.emailTextField]
+        let minimumLengthRequireMents: [UITextField : Int] = [
+            self.nameTextField : 1,
+            self.emailTextField : 6
+        ]
+            
+        
+        // 2. iterrate over the text fields
+        for textField in textFields {
+            
+            // 3. if the textfield doesn't have the minimum required characters...
+            if !self.textField(textField, hasMinimumCharacters: minimumLengthRequireMents[textField]!) {
+                
+                // 4. make sure that the label isn't hidden
+                if self.errorLabel.isHidden {
+                    self.errorLabel.isHidden = false
+                }
+                
+                // 5. display an error to rhe user
+                self.errorLabel.text = "\(textField.debugId) does not meet the minimum character count of \(minimumLengthRequireMents[textField]! as Int)"
+                
+                // 6. indicate that the fields are not valid
+                return false
+            }
+        }
+
+        // 3. hide the error label if all gets validated
+        if !self.errorLabel.isHidden {
+            self.errorLabel.isHidden = true
+        }
+        
+        // 4. indicate that the fields are valid
+        return true
+    }
+    
+    func textField(_ textField: UITextField, hasMinimumCharacters minimum: Int) -> Bool {
+        
+        guard let currentText: String = textField.text else {
+            return false
+        }
+        
+        
+        if currentText.characters.count >= minimum {
+            return true
+        }
+        
+        return false
     }
     
     
@@ -37,29 +91,34 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     // we can take a look and get a general sense of what happens, when
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         // the .debugId property is defined in an extension, it's not actually part of UITextField
-        print("\n + \(textField.debugId) SHOULD BEGIN")
+//        print("\n + \(textField.debugId) SHOULD BEGIN")
         return true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("\n + \(textField.debugId) DID BEGIN")
+//        print("\n + \(textField.debugId) DID BEGIN")
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("\n - \(textField.debugId) SHOULD END")
+//        print("\n - \(textField.debugId) SHOULD END")
         return true
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        print("\n - \(textField.debugId) DID END")
+//        print("\n - \(textField.debugId) DID END")
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("\n ~ \(textField.debugId) SHOULD RETURN")
+        
+        _ = self.textFieldsAreValid()
+        
         return true
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        
         
         return true
     }
