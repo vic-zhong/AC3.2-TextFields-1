99fcf589595f18e8dc0ed51a3bbd2aa7c55c904c
Louis Tur <louis.tur@gmail.com>
2016-10-02 23:08:07 -0400

    Updates storyboard to better reflect lesson instructions

--- CatRoll-SignUp/View Controllers/LoginViewController.swift
+++ CatRoll-SignUp/View Controllers/LoginViewController.swift
@@ -8,12 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var errorLabel: UILabel!
-    
+class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,22 +22,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    @IBAction func didTapLogin(_ sender: UIButton) {
-        print("did tap button")
-        print("Textfields are valid: \(textFieldsAreValid())")
-    }
-    
-    
     // MARK: - Validations
     func textFieldsAreValid() -> Bool {
         
         // 1. some set up
-        let textFields: [UITextField] = [self.nameTextField, self.passwordTextField]
-        let minimumLengthRequireMents: [UITextField : Int] = [
-            self.nameTextField : 1,
-            self.passwordTextField : 6
-        ]
-        
 
         // 2. iterrate over the text fields
 
@@ -73,45 +56,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     
     // MARK: - UI Helper
-    // (add the label update function here)
+    // (add the label update function here when the lesson calls for it)
     
     
     // MARK: - UITextFieldDelegate
-    
-    // we can take a look and get a general sense of what happens, when
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        
-        // the .debugId property is defined in an extension, it's not actually part of UITextField
-        print("\n + \(textField.debugId) SHOULD BEGIN")
-        return true
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("\n + \(textField.debugId) DID BEGIN")
-    }
-    
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("\n - \(textField.debugId) SHOULD END")
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        print("\n - \(textField.debugId) DID END")
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("\n ~ \(textField.debugId) SHOULD RETURN")
-        return true
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        // only interested in doing this validation for self.nameTextField
-        // and per documentation, string can be empty if the change is a deletion
-        if textField == self.nameTextField && string != "" {
-            return self.string(string, containsOnly: CharacterSet.letters.union(CharacterSet.whitespaces))
-        }
-        
-        return true
-    }
+    // (add delegate functions below here)
 }
